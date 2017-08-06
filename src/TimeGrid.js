b126e1992f3baf6d3de213f308ce77cdd0a36870
Richard Foo <foo.richard@gmail.com>
2017-08-06 18:01:24 -0400

    Try removing all day again.

--- src/TimeGrid.js
+++ src/TimeGrid.js
@@ -244,6 +244,11 @@ export default class TimeGrid extends Component {
         style={style}
       >
         <div className='rbc-row'>
+          <div
+            ref={ref => this._gutters[0] = ref}
+            className='rbc-label rbc-header-gutter'
+            style={{ width }}
+          />
           { this.renderHeaderCells(range) }
         </div>
       </div>
